8a097c5e9bf6bcd694c20c91b62eb986ab7ebbb6
Matěj Schrödl <matej.schrodl@gmail.com>
2025-02-16 15:15:00 +0100

    Better grid

--- game.js
+++ game.js
@@ -4,17 +4,15 @@ function resizeCanvasToWindow(canvas) {
   canvas.height = window.innerHeight;
 }
 
-function drawGrid(rows, cols, cellSize) {
 
-  const gridWidth = cols * cellSize;
-  const gridHeight = rows * cellSize;
-
-  const offsetX = (canvas.width - gridWidth) / 2;
-  const offsetY = (canvas.height - gridHeight) / 2;
+function drawGrid(canvas, rows, cols) {
+  
+  const cellWidth = canvas.width / cols;  
+  const cellHeight = canvas.height / rows;
 
   for(let row = 0; row < rows; row++) {
     for(let col = 0; col < cols; col++) {
-      ctx.strokeRect(offsetX + col*10, offsetY + row*10, 10,10);
+      ctx.strokeRect(col*cellWidth,row*cellHeight, cellWidth,cellHeight);
     }
   }
 }
@@ -28,10 +26,11 @@ const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 const rows = 40;
 const cols = 40;
-const cellSize = 10;
 
 window.addEventListener('resize', onWindowResize);
 
+const active = new Map();
+//active.set({1,2}, true);
 
 resizeCanvasToWindow(canvas);
-drawGrid(canvas);
+drawGrid(canvas, rows, cols);
