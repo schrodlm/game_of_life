40819269b99f5b85f31d9224d811bca47a3c12c7
Matěj Schrödl <matej.schrodl@gmail.com>
2025-02-16 15:36:16 +0100

    Active cells

--- game.js
+++ game.js
@@ -4,8 +4,14 @@ function resizeCanvasToWindow(canvas) {
   canvas.height = window.innerHeight;
 }
 
+class Position {
+  constructor(x,y) {
+    this.x = x;
+    this.y = y;
+  }
+}
 
-function drawGrid(canvas, rows, cols) {
+function drawGrid(topLeft, canvas, rows, cols, active) {
   
   const cellWidth = canvas.width / cols;  
   const cellHeight = canvas.height / rows;
@@ -15,12 +21,25 @@ function drawGrid(canvas, rows, cols) {
       ctx.strokeRect(col*cellWidth,row*cellHeight, cellWidth,cellHeight);
     }
   }
+
+  // Looping through active cells
+  for(const pos of active) {
+    const realX = pos.x - topLeft.x;
+    const realY = pos.y - topLeft.y;
+
+    if(realX >= 0 && realX < cols && realY >= 0 && realY < rows) {
+      ctx.fillRect(realX*cellWidth, realY*cellHeight, cellWidth, cellHeight);
+    } 
+  }
+
+
 }
 
 function onWindowResize(canvas) {
   resizeCanvasToWindow(canvas);
   drawGrid();
 }
+
 //----------------------------------------------------------------------
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
@@ -29,8 +48,9 @@ const cols = 40;
 
 window.addEventListener('resize', onWindowResize);
 
-const active = new Map();
-//active.set({1,2}, true);
+const active = new Set();
+pos = new Position(3,4)
+active.add(new Position(3,4));
 
 resizeCanvasToWindow(canvas);
-drawGrid(canvas, rows, cols);
+drawGrid(new Position(0,0), canvas, rows, cols, active);
